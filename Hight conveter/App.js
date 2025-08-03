3b75665db2d32981c31cfc8853158ad5b55f239f
Antor-Adhikary1216 <antoradhikarygithub@gmail.com>
2025-08-04 00:50:24 +0530

    Scripting

---
+++ Hight conveter/App.js
@@ -0,0 +1,16 @@
+const form = document.querySelector( " form");
+form.addEventListener( " submit", function( e){ 
+
+    let feeInput = document.querySelector( "#feet")
+    let inchesInput = document.querySelector("inches")
+
+    const results = document.querySelector
+    ( "#results");
+
+
+    e.preventDefault( );
+
+    let feet =  parseInt( feeInput.value);
+    let inches =  parseInt(inchesInput.value);
+
+})
